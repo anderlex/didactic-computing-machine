ee57464f2f7898b347714a28674986dadf2b134b
anderlex <aleksandr.xvan.02@mail.ru>
2023-10-23 20:49:39 +0700

    lesson 1

--- iosSchool_HH/ViewController.swift
+++ iosSchool_HH/ViewController.swift
@@ -12,8 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        var color = Color(red: 1, green: 1, blue: 1)
+        color.alpha = .bright
+
+        var nextColor = color
+        nextColor.alpha = .average
+
+        let generator = ColorGenerator(alpha: 0.5)
+        generator.alpha = 1.0
+
+        let nextGenerator = generator
+        nextGenerator.alpha = 0.0
+
+        let alpha: Brightness = .average
+        let description = alpha.description()
     }
-
-
 }
-
