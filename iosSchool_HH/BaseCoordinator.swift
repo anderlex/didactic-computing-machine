d03a2860d2fc91fb76ec41d58362c79a245cb1a1
anderlex <aleksandr.xvan.02@mail.ru>
2023-11-06 21:01:37 +0700

    lesson 5

---
+++ iosSchool_HH/BaseCoordinator.swift
@@ -0,0 +1,23 @@
+//
+//  BaseCoordinator.swift
+//  iosSchool_HH
+//
+//  Created by student on 06.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class BaseCoordinator<Context> {
+    let assembly: Assembly
+    let context: Context
+
+    init(assembly: Assembly, context: Context) {
+        self.assembly = assembly
+        self.context = context
+    }
+
+    func make() -> UIViewController? {
+        fatalError("This method is abstract")
+    }
+}
---
+++ iosSchool_HH/Modules/Auth/AuthViewData.swift
@@ -0,0 +1,10 @@
+//
+//  AuthViewData.swift
+//  iosSchool_HH
+//
+//  Created by student on 06.11.2023.
+//
+
+import UIKit
+
+struct AuthViewData {}
