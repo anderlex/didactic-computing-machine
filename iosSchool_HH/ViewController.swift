2267df3a0701d2eb5df7270846573e43c5d12011
anderlex <aleksandr.xvan.02@mail.ru>
2023-11-24 21:41:16 +0700

    Revert "Merge pull request #7 from anderlex/7-homework"
    This reverts commit 52a6c741159c1b902bb5b5beb88b3457e5456f57, reversing
    changes made to 7648505c129205250dc8898ca76ad4b7f5b8a0e9.

---
+++ iosSchool_HH/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  iosSchool_HH
+//
+//  Created by student on 19.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
