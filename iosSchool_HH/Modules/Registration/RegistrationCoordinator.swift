fea5851dbbdc137119cb38a455c81c2a7e65838e
anderlex <aleksandr.xvan.02@mail.ru>
2023-11-25 20:21:14 +0700

    lesson 10

---
+++ iosSchool_HH/Modules/Registration/RegistrationCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrationCoordinator.swift
+//  iosSchool_HH
+//
+//  Created by student on 10.11.2023.
+//
+
+import UIKit
+
+class RegistrationCoordinator: BaseCoordinator<RegistrationCoordinator.Context> {
+
+    struct Context {
+        let onRegistrationSuccess: (() -> Void)?
+    }
+
+    override func make() -> UIViewController? {
+        assembly.registrationVC()
+    }
+}
---
+++ iosSchool_HH/Modules/Registration/RegistrationViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RegistrationViewController.swift
+//  iosSchool_HH
+//
+//  Created by student on 10.11.2023.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+    private let dataProvider: RegistrationDataProvider
+    var onRegistrationSuccess: (() -> Void)?
+
+    init(dataProvider: RegistrationDataProvider, onRegistrationSuccess: (() -> Void)?) {
+        self.dataProvider = dataProvider
+        self.onRegistrationSuccess = onRegistrationSuccess
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.onRegistrationSuccess?()
+    }
+}
